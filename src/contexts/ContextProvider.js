c73ca9b1100411ab1d13c2604881e569e7816b85
khaled-hoshan <111223587+khaled-hoshan@users.noreply.github.com>
2023-11-26 21:03:39 +0300

    .

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,11 +11,22 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+  const [screenSize, setScreenSize] = useState(undefined);
+
   return (
     <StateContext.Provider
       value={{
         activeMenu,
         setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
       }}
     >
       {children}
@@ -24,4 +35,3 @@ export const ContextProvider = ({ children }) => {
 };
 
 export const useStateContext = () => useContext(StateContext);
-
